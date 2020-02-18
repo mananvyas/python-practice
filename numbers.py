51ad45df01be49e97824361fc735f9944b2c426b
Manan <mananvyas@gmail.com>
2020-02-18 11:18:01 +0530

    First Commit

---
+++ numbers.py
@@ -0,0 +1,7 @@
+num1 = 10
+print(num1)
+
+#floating point numbers
+f1 = 6.5
+print(f1)
+print(float(f1))
---
+++ strings.py
@@ -0,0 +1,8 @@
+str1 = 'singlequote string'
+print(str1)
+
+str2 = "double quote string"
+print(str2)
+
+together = str1 + "--" + str2
+print(together)
